f298d65cdaa4e14fa2c2ff157456f6899ad63f19
ahmedehab311 <104663084+ahmedehab311@users.noreply.github.com>
2024-05-21 01:11:09 +0300

    Update recipes.js

--- src/Component/recipes.js
+++ src/Component/recipes.js
@@ -1,19 +1,27 @@
-import React ,{useState}from "react";
+import React, { useState } from "react";
 import "./recipesMoudle.css";
-function Recipes({ title, calories, image, ingredients,showIngredients,toggleIngredients   }) {
-  
-    const [show, setShow] = useState(false);
-    const toggleShow = () => {
-        setShow(!show);
-      };
-    return (
+function Recipes({
+  title,
+  calories,
+  image,
+  ingredients,
+  showIngredients,
+  toggleIngredients,
+}) {
+  const [show, setShow] = useState(false);
+  const toggleShow = () => {
+    setShow(!show);
+  };
+  return (
     <div className="recipe">
       <h1>{title}</h1>
-      <button onClick={toggleIngredients}>
-    {showIngredients ? "Hide Ingredients" : "Show Ingredients"}
-  </button>
-      {showIngredients && (
-        <div className="ingredients" style={{ opacity: show ? '0' : '1' }}>
+
+      <button onClick={toggleShow}>
+        {show ? "Hide Ingredients" : "Show Ingredients"}
+      </button>
+
+      {show && (
+        <div className="ingredients">
           <ul>
             {ingredients.map((ingredient, index) => (
               <li key={index}>{ingredient.text}</li>
@@ -21,8 +29,11 @@ function Recipes({ title, calories, image, ingredients,showIngredients,toggleIng
           </ul>
         </div>
       )}
- 
-      <p><div className="title">Calories:  </div>{calories}</p>
+
+      <p>
+        <div className="title">Calories: </div>
+        {calories}
+      </p>
       <img src={image} alt={title} className="image" />
     </div>
   );
